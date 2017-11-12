55f7d94f57cdde5c5406b7cd69e4233b671ce8c3
hmonnier <32518159+hmonnier@users.noreply.github.com>
2017-11-13 00:03:49 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## This function îs used to cache/calculate the inverse of a matrix
+## Set      :   Initalze the matrix for the all function (matrix == Argument of the function).
+##              Populate the variable in a environnenment different than the current environnement (using <<- operator)
+## Get      :   Retrieve the Matrix
+## SetInv   :   Calculate the inverse matrix
+## GetInv   :   Retrieve the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
 
+    invM <- NULL                    # Init var
+    
+    SetM <- function(z) {           # Init Matrix
+            x <<- z
+            invM <<- NULL           # Init Var
+           }
+    
+    GetM    <- function () x                  # Allow to get the matrix
+    
+    SetInvM <- function (inv) invM <<- inv    # Calculte the inverse of the matrix
+    
+    GetInvM <- function () invM               # Allow to get the inverse matrix
+    
+    list (SetM = SetM, GetM = GetM, SetInvM = SetInvM, GetInvM = GetInvM)
+    
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of a matrix. If the inverse exist in cache, returns the cached inverse matrix,
+## else calculate the inverse and returns the inverse matrix (by solve function)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    invM <- x$GetInvM()                         # evaluate value of the inverse matrix!! The variable value is present via <<- operator
+    if(!is.null(invM)) {                        # the inverse is not null so it exists in cache and returned
+          message("Matrix from cache")   
+          return(invM)                          # return stop the function  
+    }
+          
+    InitM <- x$GetM()                           # Init the original matrix 
+    invM <- solve(InitM, ...)                   # Calculte inverse matrix
+    x$SetInvM(invM)                             # Postulate the resukt 
+    return(invM)                               
 }
